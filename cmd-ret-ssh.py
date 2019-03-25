33710bfe2d9e14882412c216f4b912f9d440adc3
iamhrishikesh <33514308+Hrishikesh-Padhye@users.noreply.github.com>
2019-03-26 00:30:20 +0530

    Update and rename sshbotnet.py to cmd-ret-ssh.py

--- cmd-ret-ssh.py
+++ cmd-ret-ssh.py
@@ -7,6 +7,7 @@ class Client:
 		self.password = password
 		self.session = self.connect()
 	def connect(self):
+		#This function is used to connect to the address using the given credentials.
 		try:
 			s = pxssh.pxssh()
 			s.login(self.host, self.user, self.password)
@@ -15,27 +16,31 @@ class Client:
 			print e
 			print '[-] Error Connecting'
 	def send_command(self, cmd):
+		#This function sends the given command to the system for obtaining output.
 		self.session.sendline(cmd)
 		self.session.prompt()
 		return self.session.before
 
 def botnetCommand(command):
+		#This function executes the specified command and returns the console output.
 		for client in botNet:
 			output = client.send_command(command)
 			print '[*] Output from ' + client.host
 			print '[+] ' + output + '\n'
 
 def addClient(host, user, password):
+	#This function adds a client machine to our list of target machines.
 	client = Client(host, user, password)
 	botNet.append(client)
 	
 botNet = []
-addClient('127.0.0.1', 'root', 'toor')
+addClient('127.0.0.1', 'root', 'toor')	#Trying on localhost
 #addClient('10.10.10.120', 'root', 'toor')
 #addClient('10.10.10.130', 'root', 'toor')
 botnetCommand('uname -v')
 botnetCommand('lscpu')
-botnetCommand('ls -a')
+#botnetCommand('ls -a')
+#botnetCommand('pwd')
 
 	
 	
